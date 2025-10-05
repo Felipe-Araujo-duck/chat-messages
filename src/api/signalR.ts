21687011afd2c3d75301efe7da9b02623647156f
Joao Marcelo Batista Narciso <joaomarcelobatistanarciso@gmail.com>
2025-10-05 14:26:46 -0300

    fix: adicionando métodos do signarlR

--- src/api/signalR.ts
+++ src/api/signalR.ts
@@ -4,17 +4,13 @@ let connection: signalR.HubConnection | null = null;
 
 export function createConnection(userId: string) {
   connection = new signalR.HubConnectionBuilder()
-    .withUrl(`https://allowing-killdeer-wise.ngrok-free.app/chat-messages/api/chat-hub?userId=${userId}`, {
-      skipNegotiation: true,
-      transport: signalR.HttpTransportType.WebSockets
-    })
+    .withUrl(`https://allowing-killdeer-wise.ngrok-free.app/chat-messages/api/chat-hub?userId=${userId}`)
     .withAutomaticReconnect()
     .build();
 
+  // Eventos recebidos do servidor
   connection.on("NotifyReceiver", (creatorUserId: number, chatId: number) => {
-    console.log(
-      `[Sistema] Notificação de solicitação de chat do usuário ${creatorUserId} para o chat ${chatId}...`
-    );
+    console.log(`[Sistema] Solicitação de chat do usuário ${creatorUserId} para o chat ${chatId}.`);
   });
 
   connection.on("NotificationAccepted", () => {
@@ -26,14 +22,15 @@ export function createConnection(userId: string) {
   });
 
   connection.on("ReceiveMessage", (senderUserId: string, message: string) => {
-    console.log(`[Mensagem de UserId: ${senderUserId}] ${message}`);
+    console.log(`[Mensagem de ${senderUserId}] ${message}`);
   });
 
   return connection;
 }
 
 export async function startConnection(userId: string) {
-  if (connection) return connection; // já existe
+  if (connection && connection.state === signalR.HubConnectionState.Connected)
+    return connection;
 
   connection = createConnection(userId);
 
@@ -55,4 +52,41 @@ export function stopConnection() {
     connection.stop();
     connection = null;
   }
+}
+
+export async function joinChat(userId: number, targetId: number, chatId?: number, accepted?: boolean) {
+  if (!connection) throw new Error("Conexão não iniciada");
+
+  await connection.invoke(
+    "JoinChat",
+    userId,
+    targetId,
+    chatId ?? 0,
+    accepted ?? false
+  );
+}
+
+export async function sendMessage(chatId: number, message: string) {
+  if (!connection) throw new Error("Conexão não iniciada");
+  if (connection.state !== signalR.HubConnectionState.Connected)
+    throw new Error("Conexão com o servidor não está ativa!");
+
+  await connection.send("SendMessage", chatId, message);
+}
+
+export async function registerPublicKey(publicKey: string, chatId: number) {
+  if (!connection) throw new Error("Conexão não iniciada");
+
+  await connection.invoke("RegisterPublicKey", publicKey, chatId);
+}
+
+export async function getPublicKeys(chatId: number) {
+  if (!connection) throw new Error("Conexão não iniciada");
+
+  const keys = await connection.invoke<{ userId: number; publicKey: string }[]>(
+    "GetPublicKey",
+    chatId
+  );
+
+  return keys;
 }
